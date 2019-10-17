e2e15646da79651ddb8c29e0526bad0d60690bec
Hao Shi <shisoftgenius@gmail.com>
2019-10-17 17:35:33 -0400

    providing failing examples

--- tests/bump_heap.rs
+++ tests/bump_heap.rs
@@ -1,6 +1,6 @@
 extern crate nulloc;
 
-// use nulloc::bump_heap::BumpAllocator;
+ use nulloc::bump_heap::BumpAllocator;
 
-// #[global_allocator]
-// static INTERNAL_ALLOCATOR: BumpAllocator = BumpAllocator;
+ #[global_allocator]
+ static INTERNAL_ALLOCATOR: BumpAllocator = BumpAllocator;
