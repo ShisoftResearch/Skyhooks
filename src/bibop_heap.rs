24ac7e71b067339c1c8ec3d78c647bd0881d69ad
Hao Shi <shisoftgenius@gmail.com>
2019-10-29 03:28:29 -0400

    env init

--- src/bibop_heap.rs
+++ src/bibop_heap.rs
@@ -3,6 +3,8 @@ use crate::collections::fixvec::FixedVec;
 use crate::collections::lflist;
 use crate::generic_heap::ObjectMeta;
 use crate::utils::{current_numa, current_thread_id};
+use core::mem::MaybeUninit;
+use core::mem;
 
 const NUM_SIZE_CLASS: usize = 16;
 const CACHE_LINE_SIZE: usize = 64;
@@ -18,11 +20,14 @@ lazy_static! {
 }
 
 struct ThreadMeta {
+    sizes: TSizeClasses,
     numa: usize,
     tid: usize,
 }
 
-struct NodeMeta {}
+struct NodeMeta {
+    free_list: lflist::List
+}
 
 struct SizeClass {
     size: usize,
@@ -55,6 +60,7 @@ impl Heap {
 impl ThreadMeta {
     pub fn new() -> Self {
         Self {
+            sizes: size_classes(),
             numa: current_numa(),
             tid: current_thread_id(),
         }
@@ -68,11 +74,35 @@ impl Drop for ThreadMeta {
     }
 }
 
+impl SizeClass {
+    pub fn new(tier: usize) -> Self {
+        Self {
+            size: tier,
+            free_list: lflist::List::new()
+        }
+    }
+}
+
 fn gen_numa_node_list() -> FixedVec<NodeMeta> {
     let num_nodes = *utils::NUM_NUMA_NODES;
     let mut nodes = FixedVec::new(num_nodes);
     for i in 0..num_nodes {
-        nodes[i] = NodeMeta {}
+        nodes[i] = NodeMeta {
+            free_list: lflist::List::new()
+        }
     }
-    unimplemented!()
+    return nodes;
+}
+
+fn size_classes() -> TSizeClasses {
+    let mut data: [MaybeUninit<SizeClass>; NUM_SIZE_CLASS] = unsafe { MaybeUninit::uninit().assume_init() };
+    let mut tier = 2;
+    for elem in &mut data[..] {
+        *elem = MaybeUninit::new(SizeClass {
+            size: tier,
+            free_list: lflist::List::new()
+        });
+        tier *= 2;
+    };
+    unsafe { mem::transmute::<_, TSizeClasses>(data) }
 }
