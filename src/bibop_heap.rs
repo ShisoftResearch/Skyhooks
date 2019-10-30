a58e9ff4417fff71eb4b1a9d5f4e33f2937604c1
Hao Shi <shisoftgenius@gmail.com>
2019-10-29 23:02:30 -0400

    make size power of two

--- src/bibop_heap.rs
+++ src/bibop_heap.rs
@@ -111,9 +111,21 @@ fn size_classes() -> TSizeClasses {
 }
 
 fn per_node_heap() -> usize {
-    *SYS_TOTAL_MEM
+    min_power_of_2(*SYS_TOTAL_MEM)
 }
 
 fn total_heap() -> usize {
-    per_node_heap() * *NUM_NUMA_NODES
+    min_power_of_2(per_node_heap() * *NUM_NUMA_NODES)
+}
+
+fn min_power_of_2(n: usize) -> usize {
+    let count = 0;
+    // First n in the below condition
+    // is for the case where n is 0
+    if n > 0 && (n & (n - 1)) == 0 { return n; }
+    while n != 0 {
+        n >>= 1;
+        count += 1;
+    }
+    return 1 << count;
 }
