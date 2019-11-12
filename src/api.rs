15723e78340f08f4407e76dcec45676f5fe7aa06
Hao Shi <shisoftgenius@gmail.com>
2019-11-11 22:31:10 -0500

    revert file to stable

--- src/api.rs
+++ src/api.rs
@@ -1,43 +1,40 @@
-use crate::mmap_heap::*;
-use crate::utils::{align_padding, debug_validate};
-use crate::{bump_heap, generic_heap, Ptr, Size, NULL_PTR};
+use crate::{generic_heap, Ptr, Size, NULL_PTR, bump_heap};
 use core::alloc::{GlobalAlloc, Layout};
-use core::cell::Cell;
-use lfmap::{Map, WordMap};
 use libc::*;
+use lfmap::{Map, WordMap};
+use crate::utils::align_padding;
+use crate::mmap_heap::*;
 use std::ptr::null_mut;
+use core::cell::Cell;
 
 thread_local! {
     pub static INNER_CALL: Cell<bool> = Cell::new(false);
 }
 lazy_static! {
-    static ref RUST_ADDR_MAPPING: lfmap::WordMap<MmapAllocator> =
-        lfmap::WordMap::<MmapAllocator>::with_capacity(2048);
+    static ref RUST_ADDR_MAPPING: lfmap::WordMap<MmapAllocator> = lfmap::WordMap::<MmapAllocator>::with_capacity(2048);
 }
 
 pub unsafe fn nu_malloc(size: Size) -> Ptr {
-    if size == 0 {
-        return null_mut();
-    } // The C standard (C17 7.22.3/1)
-    let ptr = INNER_CALL.with(|is_inner| {
+    if size == 0 { return null_mut(); } // The C standard (C17 7.22.3/1)
+    INNER_CALL.with(|is_inner| {
         if !is_inner.get() {
             is_inner.set(true);
+            crate::small_heap::warm_up();
             let res = generic_heap::malloc(size);
             is_inner.set(false);
             res
         } else {
             bump_heap::malloc(size)
         }
-    });
-    debug_validate(ptr, size)
+    })
+
 }
 pub unsafe fn nu_free(ptr: Ptr) {
-    if ptr == null_mut() {
-        return;
-    }
+    if ptr == null_mut() { return; }
     INNER_CALL.with(|is_inner| {
         if !is_inner.get() {
             is_inner.set(true);
+            crate::small_heap::warm_up();
             generic_heap::free(ptr);
             is_inner.set(false);
         } else {
@@ -49,11 +46,14 @@ pub unsafe fn nu_free(ptr: Ptr) {
 pub unsafe fn nu_calloc(nmemb: Size, size: Size) -> Ptr {
     let total_size = nmemb * size;
     let ptr = nu_malloc(total_size);
-    debug_validate(ptr, total_size)
+    if ptr != NULL_PTR {
+        memset(ptr, 0, total_size);
+    }
+    ptr
 }
 
 pub unsafe fn nu_realloc(ptr: Ptr, size: Size) -> Ptr {
-    let ptr = INNER_CALL.with(|is_inner| {
+    INNER_CALL.with(|is_inner| {
         if !is_inner.get() {
             is_inner.set(true);
             let res = generic_heap::realloc(ptr, size);
@@ -62,8 +62,7 @@ pub unsafe fn nu_realloc(ptr: Ptr, size: Size) -> Ptr {
         } else {
             bump_heap::realloc(ptr, size)
         }
-    });
-    debug_validate(ptr, size)
+    })
 }
 
 // Allocator for rust itself for internal heaps
@@ -77,16 +76,13 @@ unsafe impl GlobalAlloc for NullocAllocator {
         let base_addr = nu_malloc(actual_size) as usize;
         let align_padding = align_padding(base_addr, align);
         let rust_addr = base_addr + align_padding;
-        if align_padding != 0 { RUST_ADDR_MAPPING.insert(rust_addr, base_addr); }
+        RUST_ADDR_MAPPING.insert(rust_addr, base_addr);
         rust_addr as *mut u8
     }
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
         let addr = ptr as usize;
-        let base_addr = if layout.align() == 1 {
-            addr
-        } else {
-          if let Some(real_base) = RUST_ADDR_MAPPING.remove(addr) { real_base }  else { addr }
-        };
-        nu_free(base_addr as Ptr)
+        if let Some(base_addr) = RUST_ADDR_MAPPING.remove(addr) {
+            nu_free(base_addr as Ptr)
+        }
     }
 }
