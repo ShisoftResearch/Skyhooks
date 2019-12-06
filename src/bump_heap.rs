3a24947371e6c3bc81091216aa1125fd99e4fd54
Hao Shi <shisoftgenius@gmail.com>
2019-12-06 15:14:41 -0500

    fix addressing

--- src/bump_heap.rs
+++ src/bump_heap.rs
@@ -140,8 +140,8 @@ unsafe impl<A: Alloc + Default> GlobalAlloc for AllocatorInstance<A> {
             .unwrap_or_else(|| {
                 let size_with_bookmark = actual_size + bookmark_size;
                 let addr = self.bump_allocate(size_with_bookmark);
-                addr
-            }) + bookmark_size;
+                addr + bookmark_size
+            });
         let align_padding = align_padding(origin_addr, align);
         let final_addr = origin_addr + align_padding;
         unsafe {
@@ -164,8 +164,8 @@ unsafe impl<A: Alloc + Default> GlobalAlloc for AllocatorInstance<A> {
         let origin_ptr = (size_ptr as usize - mem::size_of::<usize>()) as *mut usize;
         let record_size = ptr::read(size_ptr) - 1;
         let origin_addr = ptr::read(origin_ptr);
-        if origin_addr <= addr
-            || origin_addr > addr - maximum_free_list_covered_size()
+        if origin_addr < addr
+            || origin_addr > addr + maximum_free_list_covered_size()
             || record_size != actual_size
         {
             // fail safe, not sure how this happened though
